f264731dd7d2a3c544020a745e1574dfd6fe617f
GSandeep96 <14311a0431@sreenidhi.edu.in>
2017-01-18 18:24:14 +0530

    Fix mistakes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo Lineage_aio_otfp-user
-add_lunch_combo Lineage_aio_otfp-userdebug
-add_lunch_combo Lineage_aio_otfp-eng
+add_lunch_combo lineage_aio_otfp-user
+add_lunch_combo lineage_aio_otfp-userdebug
+add_lunch_combo lineage_aio_otfp-eng
