a4b56a7ecf1e9837c186079ffd226d36a54cbff4
BlitzFire <ninadpatilbvp@gmail.com>
2017-05-15 14:27:18 +0530

    Update dnd.mk

--- dnd.mk
+++ dnd.mk
@@ -31,7 +31,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit Lineage's custom product configuration
-$(call inherit-product, vendor/dnd/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Call device specific makefile
 $(call inherit-product, device/lenovo/aio_otfp/device.mk)
