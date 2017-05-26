7d686e315ca933026c70494a3e7186906ff66841
BlitzFire <ninadpatilbvp@gmail.com>
2017-05-26 14:16:33 +0530

    Update and rename lineage.mk to aosp_aio_otfp.mk
    aosp bringup

--- aosp_aio_otfp.mk
+++ aosp_aio_otfp.mk
@@ -37,7 +37,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 $(call inherit-product, device/lenovo/aio_otfp/device.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := lineage_aio_otfp
+PRODUCT_NAME := aosp_aio_otfp
 PRODUCT_DEVICE := aio_otfp
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo K3 Note
