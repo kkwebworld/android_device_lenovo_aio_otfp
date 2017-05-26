19e610c357104fbc104a7eb62c1ce0f4d9f10622
PainKiller3 <ninadpatil100@gmail.com>
2017-05-26 14:52:18 +0000

    fixed error

--- aosp_aio_otfp.mk
+++ aosp_aio_otfp.mk
@@ -1,12 +1,11 @@
-
 #
-# Copyright 2017 The LineageOS Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,31 +14,26 @@
 # limitations under the License.
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := Lenovo K3 Note
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
-# Inherit from AOSP 64-bit support
+# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-# Inherit from AOSP product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Inherit Lineage's custom product configuration
-$(call inherit-product, vendor/aosp/common.mk)
-
-# Call device specific makefile
+# Inherit from aio_row device
 $(call inherit-product, device/lenovo/aio_otfp/device.mk)
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := aosp_aio_otfp
+# Inherit some common aosp stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := aio_otfp
+PRODUCT_NAME := aosp_aio_otfp
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := Lenovo K3 Note
 PRODUCT_MANUFACTURER := Lenovo
-PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1920
+TARGET_SCREEN_HEIGHT := 1080
+
+$(call inherit-product-if-exists, vendor/lenovo/aio_otfp/aio_otfp-vendor.mk)
