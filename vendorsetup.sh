3321f026c50671cd12b925b9ea2e53bf0820d8f3
BlitzFire <ninadpatilbvp@gmail.com>
2017-05-26 14:21:50 +0530

    Update vendorsetup.sh
    aosp bring up

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,1 @@
-add_lunch_combo lineage_aio_otfp-userdebug
-add_lunch_combo aim_aio_otfp-userdebug
-add_lunch_combo dnd_aio_otfp-userdebug
-add_lunch_combo aicp_aio_otfp-userdebug
+add_lunch_combo aosp_addison-userdebug
